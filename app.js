bbbdb5ae906a9abb6ba42a83d1f706f5e78455a1
agnaldofondo <92089307+agnaldofondo@users.noreply.github.com>
2021-10-15 18:42:42 +0200

    last round on dalima

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const {WriteFileSync, writeFileSync} = require('fs')
-for (let i = 0; i <1000; i++) {
-    writeFileSync('./content/big.txt', `hello word ${i}\n`, {flag: 'a'})
-}
+var http = require('http')
+var fs = require('fs')
+
+http
+.createServer(function(req, res){
+//     const text =  fs.readFileSync('./content/big.txt','utf8')
+
+// res.end(text)
+const fileStream = fs.createReadStream('./content/big.txt','utf8')
+fileStream.on('open',()=>{
+    fileStream.pipe(res)
+})
+fileStream.on('error',(err)=>{
+    res.end(err)
+})
+})
+.listen(5000)
